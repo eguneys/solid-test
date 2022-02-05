25a1202708ab3bb823957a9c2ff488eabca21e32
Emre Guneyler <eguneys@hotmail.com>
2022-02-05 03:34:42 +0000

    node resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 const htmlTemplate = require('rollup-plugin-generate-html-template')
 const typescript = require('@rollup/plugin-typescript')
 
@@ -7,6 +8,7 @@ export default {
     { format: 'iife', name: 'Space', dir: 'dist' }
   ],
   plugins: [
+    nodeResolve(),
     typescript(),
     htmlTemplate({
       template: 'src/index.html',
