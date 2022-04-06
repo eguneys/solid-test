148fcd29d9be8eae40fd37ebd3bb3762ae338b80
Emre Guneyler <eguneys@gmail.com>
2022-04-06 19:48:09 +0300

    input

--- src/app2.tsx
+++ src/app2.tsx
@@ -1,8 +1,11 @@
 import { on, createEffect, createSignal, createContext, useContext } from 'soli2d-js'
 import { Quad, Vec2, loop } from 'soli2d'
+import Input from './input'
 
 const colors = {
-  red2: [4, 2]
+  sand2: [3, 1],
+  red2: [4, 2],
+  red3: [4, 3]
 }
 
 const AppContext = createContext<AppContextValue>({})
@@ -11,15 +14,20 @@ const AppProvider = (props) => {
   const [image, _setImage] = createSignal()
   const [root, _setRoot] = createSignal()
   const [update, setUpdate] = createSignal([0, 0])
+  const [input, setInput] = createSignal(new Input().init(), { equals: false })
 
-    let store = [image, root, update, {
-      _setImage,
-        _setRoot
-    }]
+  let store = [{image, root, update, input}, {
+    _setImage,
+      _setRoot
+  }]
 
   loop((dt, dt0) => {
     setUpdate([dt, dt0])
+    setInput(input => {
+      input.update(dt, dt0)
+      return input
     })
+  })
 
   return (
     <AppContext.Provider value={store}>
@@ -34,7 +42,7 @@ const App = (_image, _root) => {
 
   let _App = () => {
 
-    const [image, root, update, { _setImage, _setRoot }] = useApp()
+    const [{image, root, update}, { _setImage, _setRoot }] = useApp()
 
       _setImage(_image)
       _setRoot(_root)
@@ -62,29 +70,54 @@ export const Game = () => {
 
   
   return (<>
-      <StrokeRect color={colors.red2} x={50} y={50} w={20} h={20} thick={5}/>
-      <StrokeRect color={colors.red2} x={71} y={50} w={20} h={20} thick={5}/>
-      <StrokeRect color={colors.red2} x={50} y={71} w={20} h={20} thick={5}/>
+      <Background/>
+      <Mila/>
+      <StrokeRect color={colors.red2} x={50} y={50} w={20} h={20}/>
+      <StrokeRect color={colors.red2} x={71} y={50} w={20} h={20}/>
+      <StrokeRect color={colors.red2} x={50} y={71} w={20} h={20}/>
     </>)
+}
+
+export const Mila = () => {
+
+  let [{input}] = useApp()
+
+  let [hue, lum] = colors.red3
+
+  createEffect(on(input, (input) => {
+    console.log(input.btn('a'))
+        }))
+
+  return (<transform rotation={Math.PI*0.25} pivot={Vec2.make(0.5, 0.5)} x={10} y={10}>
+      <Tile lum={lum} hue={hue} pivot={0.5} size={Vec2.make(14, 14)} x={0} y={0}/>
+      </transform>)
+}
+
+export const Background = () => {
+  let [hue, lum] = colors.sand2
+  return (<>
+      <Tile lum={lum} hue={hue} size={Vec2.make(320, 180)} x={0} y={0}/>
+      </>)
 }
 
 
 export const StrokeRect = (props) => {
 
+  let thick = props.thick || 2
   let [hue, lum] = props.color
 
     return (<transform x={props.x} y={props.y}>
-        <Tile lum={lum} hue={hue} size={Vec2.make(props.w, props.thick)} x={0} y={0}/>
-        <Tile lum={lum} hue={hue} size={Vec2.make(props.w, props.thick)} x={0} y={props.h-props.thick}/>
-        <Tile lum={lum} hue={hue} size={Vec2.make(props.thick, props.h)} x={0} y={0}/>
-        <Tile lum={lum} hue={hue} size={Vec2.make(props.thick, props.h)} x={props.w-props.thick} y={0}/>
+        <Tile lum={lum} hue={hue} size={Vec2.make(props.w, thick)} x={0} y={0}/>
+        <Tile lum={lum} hue={hue} size={Vec2.make(props.w, thick)} x={0} y={props.h-thick}/>
+        <Tile lum={lum} hue={hue} size={Vec2.make(thick, props.h)} x={0} y={0}/>
+        <Tile lum={lum} hue={hue} size={Vec2.make(thick, props.h)} x={props.w-thick} y={0}/>
         </transform>)
 
 }
 
 export const Tile = (props) => {
 
-  const [image, root] = useApp()
+  const [{image, root}] = useApp()
 
   const pivot = props.pivot || 0
 
--- src/input.ts
+++ src/input.ts
@@ -1,5 +1,30 @@
 import { ticks } from './shared'
 
+let RE = /^[A-Za-z0-9\+\-;'\\\[\]]$/
+let RE2 = /^(\s|Shift|ArrowUp|ArrowDown|ArrowLeft|ArrowRight|Backspace|Enter|Tab|\*)$/
+let RE3 = /^(\!|\$)$/
+function capture_key(key: string) {
+  return key.match(RE) || key.match(RE2) || key.match(RE3)
+}
+
+function appr(value: number, target: number, dt: number)  {
+  if (value < target) {
+    return Math.min(value + dt, target)
+  }else {
+    return Math.max(value - dt, target)
+  }
+}
+
+type ButtonState = {
+  just_pressed: boolean,
+  just_released: boolean,
+  ctrl: boolean,
+  shift: boolean,
+  t: number,
+  t0: number
+}
+
+
 export default class Input {
 
 
@@ -12,71 +37,129 @@ export default class Input {
   }
 
 
-  _btn = new Map()
+  _btn = new Map<string, ButtonState>()
 
-  private press = (key: string) => {
-    if (!this._btn.has(key) || this._btn.get(key) === 0) {
-      this._btn.set(key, ticks.one)
+  _last_released?: string
+  _btnpp?: string
+
+  private press = (key: string, e: KeyEvent) => {
+    
+    let ctrl = e.ctrlKey,
+      shift = e.shiftKey
+
+    if (!this._btn.has(key)) {
+      this._btn.set(key, {
+        just_pressed: true,
+        just_released: false,
+        ctrl,
+        shift,
+        t: 0,
+        t0: 0
+      })
+      return
     }
+    this._btn.get(key)!.just_pressed = true
   }
 
 
   private release = (key: string) => {
-    this._btn.set(key, -ticks.three)
+    let res = this._btn.get(key)
+    if (res) {
+      res.just_released = true
+    }
   }
 
-  btn = (key: string) => {
-    return this._btn.get(key) || 0
+  btn = (key: string, shift?: boolean, ctrl?: boolean) => {
+    let btn = this._btn.get(key)
+
+    if (btn && !!shift === !!btn.shift) {
+      return btn.t
+    } else {
+      return 0
+    }
   }
 
-  update = (dt: number, dt0: number) => {
-    for (let [key, t] of this._btn) {
-      let sign = Math.sign(t)
-      if (t !== 0) {
-        t += dt
-        if (Math.sign(t) !== sign) {
-          t = 0
-        }
-      }
-      this._btn.set(key, t)
+  btn0 = (key: string, shift?: boolean, ctrl?: boolean) => {
+    let btn = this._btn.get(key)
+
+    if (btn && !!shift === !!btn.shift) {
+      return btn.t0
+    } else {
+      return 0
     }
   }
 
 
-  constructor() {
+  btnp = (key: string, shift?: boolean, ctrl?: boolean) => {
+    return this.btn(key, shift, ctrl) > 0 && this.btn0(key, shift, ctrl) === 0
+  }
+
+  btnpp = (key: string) => {
+    return this._btnpp === key
+  }
+
+  update = (dt: number, dt0: number) => {
+
+    this._btnpp = undefined
+
+    for (let [key, s] of this._btn) {
+      if (s.t === 0) {
+        s.t0 = s.t
+      }
+      if (s.just_pressed || s.t > 0) {
+        s.t0 = s.t
+        s.t += dt
+        s.just_pressed = false
+      }
+
+      if (s.just_released) {
+        s.t0 = s.t
+        s.t = 0
+        s.just_released = false
+
+        if (this._last_released && this._last_released === key) {
+          this._btnpp = key
+          this._last_released = undefined
+        } else {
+          this._last_released = key
+        }
+      }
+    }
+  }
+
+  init() {
 
     let { press, release } = this
 
     document.addEventListener('keydown', e => {
+      if (e.ctrlKey || !capture_key(e.key)) {
+        return
+      }
+      e.preventDefault()
       switch(e.key) {
         case 'ArrowUp':
-          press('up');
+          press('up', e);
           break;
         case 'ArrowDown':
-          press('down');
+          press('down', e);
           break;
         case 'ArrowLeft':
-          press('left');
+          press('left', e);
           break;
         case 'ArrowRight':
-          press('right');
+          press('right', e);
           break;
-        case 'x':
-          press('x');
-          break;
-        case 'c':
-          press('c');
-          break;
-        case 'z':
-          press('z')
-          break;
-        case 'e':
-          press('e')
-          break;
+        default:
+          press(e.key, e)
+          break
       }
     });
 
     document.addEventListener('keyup', e => {
+      if (e.ctrlKey || !capture_key(e.key)) {
+        return
+      }
+      e.preventDefault()
       switch(e.key) {
         case 'ArrowUp':
           release('up');
@@ -90,19 +173,12 @@ export default class Input {
         case 'ArrowRight':
           release('right');
           break;
-        case 'c':
-          release('c');
-          break;
-        case 'x':
-          release('x');
-          break;      
-        case 'z':
-          release('z');
-          break;
-        case 'e':
-          release('e')
+        default:
+          release(e.key)
           break
       }
     });
+
+    return this
   }
 }
